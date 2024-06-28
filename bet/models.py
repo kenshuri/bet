b116f431a765ed6fc61af42a5bbc932d858b964e
alexis <mailalesci@gmail.com>
2024-06-28 16:39:27 +0200

    FEAT: improve admin

--- bet/models.py
+++ bet/models.py
@@ -7,6 +7,9 @@ from django.core.validators import MinValueValidator
 class Team(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return f"{self.name}"
+
 class Game(models.Model):
     euro_number = models.IntegerField(unique=True)
     team_1 = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='team_1')
@@ -15,11 +18,17 @@ class Game(models.Model):
     score_team2 = models.IntegerField(null=True, blank=True)
     start_datetime = models.DateTimeField()
 
+    def __str__(self):
+        return f"{self.euro_number}: {self.team_1} vs {self.team_2}"
+
 
 class League(models.Model):
     name = models.CharField(max_length=100)
     users = models.ManyToManyField(CustomUser, related_name='users')
     owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='owner')
+
+    def __str__(self):
+        return f"{self.name}"
 
 class Bet(models.Model):
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
@@ -30,3 +39,6 @@ class Bet(models.Model):
 
     class Meta:
         unique_together = ('game', 'user', 'league')
+
+    def __str__(self):
+        return f"{self.user} bet on {self.game} in league {self.league}"
